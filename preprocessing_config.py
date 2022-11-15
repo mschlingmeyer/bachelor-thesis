f41e378fe90aa5d2a9c1b46e2734b56627d13d9b
Maja Schlingmeyer <maja.schlingmeyer@t-online.de>
2022-11-15 14:14:40 +0100

    added backgrounds tt and tth

--- preprocessing_config.py
+++ preprocessing_config.py
@@ -1,12 +1,16 @@
 label_config = {
     "hh_ggf": 1,
     "dy": 0,
-    #"tt": 0
+    "tt": 0,
+    "tth":0
 }
 
 
 input_directories = [
     "/afs/desy.de/user/s/schlingm/nfs/2018__dy",
     "/afs/desy.de/user/s/schlingm/nfs/2018__hh_ggf_kl1_kt1",
-    #"/afs/desy.de/user/s/schlingm/nfs/2018__tt_sl"
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tt_sl",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tt_fh",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tt_dl",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tth_bb"
 ]
