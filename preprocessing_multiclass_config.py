107a43fa76c312fb57f922360fc32bdfc9767f97
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2022-12-20 16:19:18 +0100

    new preprocessing scripts to create inputs for multiclass trainings

---
+++ preprocessing_multiclass_config.py
@@ -0,0 +1,26 @@
+label_config = {
+    "hh_ggf": 1,
+    "dy": 2,
+    "tt_sl": 3,
+    "tt_fh":3,
+    "tt_dl":3,
+    "tth":4
+}
+
+# label_classes = [0,1]
+
+label_names = {
+    "1": "ggF->HH",
+    "2": "DY",
+    "3": r"t\bar{t}",
+    "4": r"t\bar{t}H",
+}
+
+input_directories = [
+    "/afs/desy.de/user/s/schlingm/nfs/2018__dy",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__hh_ggf_kl1_kt1",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tt_sl",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tt_fh",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tt_dl",
+    "/afs/desy.de/user/s/schlingm/nfs/2018__tth_bb"
+]
